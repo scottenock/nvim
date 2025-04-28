0ace921c5edfae1f3e1f0f750b8a9e437adb125e
scottenock <scottenock@enock.ovh>
2025-04-28 13:13:02 +0100

    FEAT: add git conflict-marker

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,4 +46,32 @@ return {
       },
     },
   },
+
+  {
+    "rhysd/conflict-marker.vim",
+    event = "VeryLazy", -- or choose a better event if you prefer
+    config = function()
+      -- Disable default highlight group
+      vim.g.conflict_marker_highlight_group = ""
+
+      -- Customize regex patterns for conflict markers (optional)
+      vim.g.conflict_marker_begin = "^<<<<<<<\\+ .*$"
+      vim.g.conflict_marker_common_ancestors = "^|||||||\\+ .*$"
+      vim.g.conflict_marker_end = "^>>>>>>>\\+ .*$"
+
+      -- Define your own highlight groups
+      vim.api.nvim_set_hl(0, "ConflictMarkerBegin", { bg = "#2f7366" })
+      vim.api.nvim_set_hl(0, "ConflictMarkerOurs", { bg = "#2e5049" })
+      vim.api.nvim_set_hl(0, "ConflictMarkerTheirs", { bg = "#344f69" })
+      vim.api.nvim_set_hl(0, "ConflictMarkerEnd", { bg = "#2f628e" })
+      vim.api.nvim_set_hl(0, "ConflictMarkerCommonAncestorsHunk", { bg = "#754a81" })
+    end,
+  },
+
+  {
+    "davidmh/mdx.nvim",
+    config = true,
+    lazy = false,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
 }
